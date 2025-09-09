dc27da4b42c34e26ffe593edb2eeae4f1f4efb00
Aissa DJENID <aissa.djenid2025@campus-eni.fr>
2025-09-09 10:13:49 +0200

    m

---
+++ app/src/main/java/com/example/demoandroid/demo_3/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.demoandroid.demo_3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.demoandroid.demo_3.ui.theme.DemoAndroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MainPage()
+        }
+    }
+}
+
+@Composable
+fun MainPage(){
+    var persons = mutableListOf(
+        "Isaac",
+        "Gregory"
+    )
+
+    DemoAndroidTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Column(modifier = Modifier.fillMaxSize().padding(innerPadding)) {
+                Text("Demo ListView")
+
+                LazyColumn {
+                    items(persons) { person ->
+                        Text(text=person)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MainPage()
+}
