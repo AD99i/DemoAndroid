0b379aab0abcbbfbfc6a3a01c0e02a6241609f8f
AD99i <zawarudo74u@gmail.com>
2025-09-10 10:16:49 +0200

    m

---
+++ app/src/main/java/com/example/demoandroid/demo_nav/PageOneActivity.kt
@@ -0,0 +1,79 @@
+package com.example.demoandroid.demo_nav
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.demoandroid.demo_nav.PageTwoActivity
+import com.example.demoandroid.ui.theme.DemoAndroidTheme
+
+class PageOneActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PageOne()
+        }
+
+    }
+}
+
+@Composable
+fun PageOne() {
+
+    //recuperer le contexte actuel de l'appli (important pour changer de page)
+    val context = LocalContext.current
+    DemoAndroidTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Gray)
+                    .padding(innerPadding)
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(40.dp)
+                ) {
+                    Text("Page 1")
+
+                    ElevatedButton(
+                        onClick = {
+                            //Le code pour changer de page
+                            val intent = Intent(context, PageTwoActivity::class.java)
+                            context.startActivity(intent)
+                        },
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text("Aller sur la page 2")
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PageOnePreview() {
+    PageOne()
+}
